954af613e0d8845d6832f7cceaed36afcd999101
WOOL <woolen_h@kakao.com>
2024-01-15 17:35:49 +0900

    [create] main - landing

---
+++ assets/js/selectFunction.js
@@ -0,0 +1,22 @@
+const label = document.querySelector('.label');
+const options = document.querySelectorAll('.navigation_item');
+
+// 클릭한 옵션의 텍스트를 라벨 안에 넣음
+const handleSelect = (item) => {
+    label.parentNode.classList.remove('active');
+    label.innerHTML = item.textContent;
+}
+
+// 옵션 클릭 시 클릭한 옵션을 넘김
+options.forEach(option => {
+    option.addEventListener('click', () => handleSelect(option))
+})
+
+//라벨을 클릭 시 옵션 목록이 열림 / 닫힘
+label.addEventListener('click', () => {
+    if(label.parentNode.classList.contains('active')) {
+        label.parentNode.classList.remove('active');
+    } else {
+        label.parentNode.classList.add('active');
+    }
+})
