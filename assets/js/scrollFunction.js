8177d2dddb3b7e298a8f6db2d3d3b07e7fd41bc3
WOOL <woolen_h@kakao.com>
2024-01-16 16:01:49 +0900

    [Fix] 제이쿼리 스크립트 경로 오류 수정

--- assets/js/scrollFunction.js
+++ assets/js/scrollFunction.js
@@ -7,36 +7,44 @@ window.addEventListener("wheel", function(e){
 // false로 설정 시 감시가 일어나지 않아 이벤트 원천 차단
 
 const mHtml = $("html");
+// 스크롤 이벤트 일어날 요소 선택
 let page = 1;
+// 뷰포트에 표시되는 페이지 번호
 const lastPage = $(".section").length;
+// 마지막 페이지의 번호 변수로 설정
 
 mHtml.animate({scrollTop : 0},10);
+// 문서가 로드되면 첫 페이지부터 시작하게끔 함
 
 $(window).on("wheel", function(e) {
     // 이벤트 핸들러로 휠을 굴리면 발생하는 이벤트
     if(mHtml.is(":animated")) return;
     // 아래에서 호출된 .animate 메서드로 생성된 스크롤 효과가 쌓이지 않도록 스크롤이 진행되는 동안 wheel 이벤트는 무시
     if(e.originalEvent.deltaY > 0) {
+        // 아래쪽으로 굴렸을 때 이벤트
         // e(jQuery 반환).originalEvent(자바스크립트에서의 이벤트).deltaY(마우스 휠을 얼마나 굴렸는지)
         // deltaY > 양수일 경우 아래쪽, 음수일 경우 위쪽으로 굴림
         if(page == lastPage) {
             page = 1;
-            // 마지막 페이지인 경우 첫 번째 페이지로 이동
+            // 마지막 페이지에서 스크롤을 아래로 했을 경우 첫 번째 페이지로 이동
         } else {
             page++;
-        // 스크롤을 아래로 했을 경우 페이지 +1
+            // 스크롤을 아래로 했을 경우 페이지 +1
         }
     } else if(e.originalEvent.deltaY < 0) {
+        // 위쪽으로 굴렸을 때 이벤트
+        // e(jQuery 반환).originalEvent(자바스크립트에서의 이벤트).deltaY(마우스 휠을 얼마나 굴렸는지)
+        // deltaY > 양수일 경우 아래쪽, 음수일 경우 위쪽으로 굴림
         if(page == 1) {
-        // 첫 번째 페이지인 경우 마지막 페이지로 이동
             page = lastPage;
+            // 첫 번째 페이지에서 스크롤을 위로 했을 경우 마지막 페이지로 이동
         } else {
             page--;
         // 스크롤을 위로 했을 경우 페이지 -1
         }
         
     }
-    let posTop =(page-1) * $(window).height();
+    let posTop =(page - 1) * $(window).height();
     // 이동한 페이지 번호에 스크롤 위치 계산
     mHtml.animate({scrollTop : posTop});
     // 계산한 위치로 이동
--- assets/js/selectFunction.js
+++ assets/js/selectFunction.js
@@ -7,19 +7,27 @@ const options = document.querySelectorAll('.navigation_item');
 // 클릭한 옵션의 텍스트를 라벨 안에 넣음
 const handleSelect = (item) => {
     label.parentNode.classList.remove('active');
+    // 라벨의 부모 요소에 있는 active 클래스를 제거
     label.innerHTML = item.textContent;
+    // 라벨의 내용을 클릭한 옵션의 텍스트로 설정
 }
 
 // 옵션 클릭 시 클릭한 옵션을 넘김
 options.forEach(option => {
+    // 각각의 옵션에 대해 
     option.addEventListener('click', () => handleSelect(option))
+    // 클릭 이벤트를 추가, 옵션이 클릭되면 handleSelect 함수 호출 및 해당 함수에 옵션을 전달함
 })
 
 //라벨을 클릭 시 옵션 목록이 열림 / 닫힘
 label.addEventListener('click', () => {
+    // 라벨이 클릭되었을 때
     if(label.parentNode.classList.contains('active')) {
+        // 라벨의 부모 요소에 active 클래스가 있으면
         label.parentNode.classList.remove('active');
+        // 제거하고
     } else {
         label.parentNode.classList.add('active');
+        // 없으면 추가
     }
 })
